5eaf363e20976e7ce5abc44f0e2d831678094b1a
kairos67 <kairosys@gmail.com>
2022-07-12 19:04:42 +0900

    mysql

---
+++ nodejs/mysql.js
@@ -0,0 +1,19 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'aws.kairos',
+  user     : 'sa',
+  password : '',
+  database : 'db'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT * from topic', function (error, results, fields) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+});
+ 
+connection.end();
+//node mysql.js
