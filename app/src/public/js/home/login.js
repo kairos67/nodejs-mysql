e56841e4bf38bc44433fa85bf52ac6d688405852
kairos67 <kairosys@gmail.com>
2022-09-05 17:04:10 +0900

    DOM

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,13 @@
 "use strict";
 
-console.log("hello world");
+const id = document.querySelector("#id"),
+    pswd = document.querySelector("#pswd"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", () => {
+    const req = {
+        id: id.value,
+        pswd: pswd.value,
+    };
+    console.log(req);
+});
