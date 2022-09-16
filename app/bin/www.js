8df1a8aa574a465045dead20573bd8b80565a8ad
Kairos Jeong <kairosys@gmail.com>
2022-09-15 18:38:24 -0700

    for test in office

--- app/bin/www.js
+++ app/bin/www.js
@@ -1,9 +1,9 @@
 //샐행파일
 "use strict";
 
-const app = require('../app');
+const app = require("../app");
 const PORT = 3001; //
 
 app.listen(PORT, () => {
-    console.log("listening on port 3001...");
-})
+  console.log("listening on port 3001...");
+});
